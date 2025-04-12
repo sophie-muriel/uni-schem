b7dcf4306cbb27fa79c0e26e251da1fdc2b7db29
Andres <arperdomo26@gmail.com>
2025-04-12 00:12:38 -0500

    feat(Day): define weekday enum for availability scheduling

---
+++ uni-schem/app/models/Day.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Day(Enum):
+    """
+    Enum representing the days of the week.
+
+    Members:
+        monday (int): Monday (1)
+        tuesday (int): Tuesday (2)
+        wednesday (int): Wednesday (3)
+        thursday (int): Thursday (4)
+        friday (int): Friday (5)
+        saturday (int): Saturday (6)
+    """
+    monday = 1
+    tuesday = 2
+    wednesday = 3
+    thursday = 4
+    friday = 5
+    saturday = 6  
