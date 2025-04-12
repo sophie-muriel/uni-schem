d838a9b91f9fa673cab062db73185d35078ab626
Sophie <miso.png@gmail.com>
2025-04-12 11:15:28 -0500

    chore(app): comment out sections in app modules as examples
    - Commented out code sections in various app modules to provide examples.
    - These commented sections are not active and are intended as reference for future development.

--- uni-schem/app/schemas/item.py
+++ uni-schem/app/schemas/item.py
@@ -1,20 +1,20 @@
-# schemas/item.py
-from pydantic import BaseModel
+# # schemas/item.py
+# from pydantic import BaseModel
 
 
-class ItemBase(BaseModel):
-    name: str
-    description: str
-    price: float
-    on_offer: bool = True
+# class ItemBase(BaseModel):
+#     name: str
+#     description: str
+#     price: float
+#     on_offer: bool = True
 
 
-class ItemCreate(ItemBase):
-    pass
+# class ItemCreate(ItemBase):
+#     pass
 
 
-class ItemResponse(ItemBase):
-    id: int
+# class ItemResponse(ItemBase):
+#     id: int
 
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
