bcf5ba531b74680c8c65b093826cbbe6b375eabd
Sophie <miso.png@gmail.com>
2025-04-11 23:00:38 -0500

    feat(app): initialize FastAPI app with basic metadata and route inclusion

---
+++ uni-schem/app/main.py
@@ -0,0 +1,14 @@
+# main.py
+from fastapi import FastAPI
+from api.v1 import item_routes
+
+app = FastAPI(
+    title="Uni-ScheM",
+    description="University Schedule Manager",
+    contact={
+        "name": "Sophie Muriel",
+        "url": "https://github.com/sophie-muriel/uni-schem"
+    }
+)
+
+app.include_router(item_routes.router, prefix="/api/v1/item")
